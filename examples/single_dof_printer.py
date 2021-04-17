62231d3513a92fa007f9fa3c2e29921940f66bd1
Antonio Sapuppo <antoniosapuppo@yahoo.it>
2021-04-17 19:18:09 +0200

    motion interface added

--- examples/single_dof_printer.py
+++ examples/single_dof_printer.py
@@ -14,17 +14,49 @@ class SCurveAxisMotion(ScurvePlanner):
         super().__init__(debug)
         self.cam_pan_range = (32, 120)
         self.cam_tilt_range = (70, 185)
-        self.q0 = [32.]
-        self.q1 = [120.]
-        self.v0 = [1.]
-        self.v1 = [5.]
-        self.v_max = 20.
-        self.a_max = 15.
-        self.j_max = 100.
+        self._q0 = [32.]
+        self._q1 = [120.]
+        self._v0 = [1.]
+        self._v1 = [5.]
+        self._v_max = 20.
+        self._a_max = 15.
+        self._j_max = 100.
         self.r_profiles = None
 
+    @property
+    def q0(self):
+        return self._q0[0]
+
+    @q0.setter
+    def q0(self, value):
+        self._q0[0] = value
+
+    @property
+    def q1(self):
+        return self._q1[0]
+
+    @q1.setter
+    def q1(self, value):
+        self._q1[0] = value
+
+    @property
+    def v0(self):
+        return self._v0[0]
+
+    @v0.setter
+    def v0(self, value):
+        self._v0[0] = value
+
+    @property
+    def v1(self):
+        return self._v1[0]
+
+    @v0.setter
+    def v1(self, value):
+        self._v1[0] = value
+
     def do_plan_trajectory(self, dt):
-        traj = self.plan_trajectory(self.q0, self.q1, self.v0, self.v1, self.v_max, self.a_max, self.j_max)
+        traj = self.plan_trajectory(self._q0, self._q1, self._v0, self._v1, self._v_max, self._a_max, self._j_max)
         dof = traj.dof
         timesteps = int(max(traj.time) / dt)
         time = np.linspace(0, max(traj.time), timesteps)
